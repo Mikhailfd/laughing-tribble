a4ef708f9f1c14e6e4f8a02647d7f6eee97434d0
Mikhailfd <60361287+Mikhailfd@users.noreply.github.com>
2020-02-02 19:06:51 +0300

    zadacha5.java

---
+++ zadacha5.java
@@ -0,0 +1,26 @@
+package jo.e.wssw.d.zanjatie2;
+
+import java.util.Scanner;
+
+public class z5 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите целое число");
+        int a = scan.nextInt();
+        System.out.println("Введите степень целого числа");
+        int n = scan.nextInt();
+        int result = 1;
+
+            for (int i = 0; i < n; ++i) {
+                result = result * a;
+
+            }
+
+
+        if (n > 0) {
+            System.out.println("Результат: " + (result));
+        } else {
+            System.out.println("Степень должна быть положительной");
+        }
+    }
+}
