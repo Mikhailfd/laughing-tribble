a049589159e6757e57fd1ea6eb6ddbe6940e9f1b
Mikhailfd <60361287+Mikhailfd@users.noreply.github.com>
2020-02-09 15:38:21 +0300

    Update Zan4zd2.java

--- Zan4zd2.java
+++ Zan4zd2.java
@@ -21,3 +21,23 @@ public class Igrok {
         System.out.println("Пройденное расстояние = " + S );
     }
 }
+
+public class Koordinati {
+    private double x, y;
+    Koordinati(double x1, double x2) {
+        x = x1;
+        y = x2;
+    }
+        double getX() {
+            return x;
+        }
+    double getY() {
+        return y;
+    }
+    void newX(double hX) {
+        x = x + hX;
+    }
+    void newY(double hY) {
+        y = y + hY;
+    }
+}
