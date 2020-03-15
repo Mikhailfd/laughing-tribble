a23fabc82cb9952276823c80548502e352813bd7
Mikhailfd <60361287+Mikhailfd@users.noreply.github.com>
2020-03-16 00:02:40 +0300

    Create Main.java

---
+++ MyProject/run/Main.java
@@ -0,0 +1,13 @@
+package project.run;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Navigation menu = new Navigation();
+
+        menu.start1();
+        menu.start2();
+        menu.start3();
+        menu.start4();
+    }
+}
