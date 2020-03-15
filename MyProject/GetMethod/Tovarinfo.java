78100535114762e42c7d89a7f45534269546ca05
Mikhailfd <60361287+Mikhailfd@users.noreply.github.com>
2020-03-15 23:45:57 +0300

    Create Tovarinfo.java

---
+++ MyProject/GetMethod/Tovarinfo.java
@@ -0,0 +1,21 @@
+package project.getmethod;
+
+public class Tovarinfo {
+    private final String name;
+    private final int cena;
+    private final int id;
+    public Tovarinfo(String name, int cena, int id) {
+        this.name = name;
+        this.cena = cena;
+        this.id=id;
+    }
+        public String getname(){
+            return name;
+        }
+    public int getcena(){
+        return cena;
+    }
+    public int getId(){
+        return cena;
+    }
+    }
